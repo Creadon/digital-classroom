6b5d1ba89b16b2cad5ecfc035123ebd8b5b6a1c9
Creadon <scubegames@gmail.com>
2020-04-25 20:36:16 +0200

    Connected to backend

--- main.js
+++ main.js
@@ -9,9 +9,31 @@ app.get('/', (req, res) => {
     res.status(200).sendFile(path.resolve(process.cwd(), 'index.html'));
 });
 
-app.get('/asset', (req, res) => {
+app.get('/get', (req, res) => {
     const name = req.query.name;
     res.status(200).sendFile(path.resolve(process.cwd(), name));
 });
+
+app.get('/boxes', (req, res) => {
+    const index = req.query.index;
+    var boxes = '';
+    var html = fs.readFileSync('box.html').toString();
+    getClasses().forEach((element) => {
+        var box = html
+            .replace('%name%', element['name'])
+            .replace('%identifier%', element['identifier'])
+            .replace('%teacher%', element['teacher']);
+        boxes += box;
+    });
+    res.status(200).send(boxes);
+});
+
+app.get('/amount', (req, res) => {
+    res.status(200).send(getClasses().length.toString());
+});
+
+function getClasses() {
+    return JSON.parse(fs.readFileSync('classes.json'))['classes'];
+}
 
 app.listen(port, () => console.log('App running (CTRL + C to stop) \n > localhost:' + port));
